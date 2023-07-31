780efe8e849b091039132024a85d716509edc1d5
Hilal Demir <126691643+hilaldemir34@users.noreply.github.com>
2023-07-31 13:54:50 +0300

    Add project files.

---
+++ Casgem_Ajax/Controllers/DefaultController.cs
@@ -0,0 +1,52 @@
+﻿using Casgem_Ajax.DAL;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text.Json.Serialization;
+
+namespace Casgem_Ajax.Controllers
+{
+    public class DefaultController : Controller
+    {
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        //public IActionResult CustomerList()
+        //{
+        //    var jsonCustomer=JsonConverter.
+        //}
+        public IActionResult CustomerList()
+        {
+            var jsonCustomer = JsonConvert.SerializeObject(context.Customers.ToList());
+            return Json(jsonCustomer);
+        }
+        [HttpPost]
+        public IActionResult AddCustomer(Customer customer)
+        {
+            context.Customers.Add(customer);
+            context.SaveChanges();
+            var values = JsonConvert.SerializeObject(customer);
+            return Json(values);
+        }
+        public IActionResult DeleteCustomer(int id)
+        {
+            var value = context.Customers.Find(id);
+            context.Customers.Remove(value);
+            context.SaveChanges();  
+            return NoContent(); 
+        }
+        public IActionResult GetCustomer(int CustomerId)
+        {
+            var values=context.Customers.Find(CustomerId);
+            var jsonValues= JsonConvert.SerializeObject(values);    
+            return Json(jsonValues);
+        }
+        public IActionResult UpdateCustomer(Customer customer)
+        {
+            context.Customers.Update(customer);
+            context.SaveChanges();
+            return NoContent(); 
+        }
+    }
+}
---
+++ Casgem_Ajax/DAL/Context.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Casgem_Ajax.DAL
+{
+    public class Context:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=LAPTOP-VFP0P7JM\\SQLEXPRESS01;initial Catalog=Casgem_AjaxDb;integrated Security=true");
+        }
+        public DbSet<Customer>Customers  { get; set; }
+    }
+}
